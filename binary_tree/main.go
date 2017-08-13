0c975d7b8b9f292a8e522d862424696ba1a388d1
romanornr <romanornr@gmail.com>
2017-08-13 15:30:36 +0200

    Binary tree: Altcoin struct

--- binary_tree/main.go
+++ binary_tree/main.go
@@ -6,47 +6,69 @@ import (
 )
 
 type Node struct {
-	Value string
-	Data  int
+	Value Altcoin
 	Left  *Node
 	Right *Node
 }
 
-func (n *Node) Insert(value string, data int) error {
+type Altcoin struct {
+	Name  string
+	Price float64
+}
+
+func (n *Node) Insert(a Altcoin) error {
 	if n == nil {
-		return errors.New("Can't insert a value into a nil tree")
+		return errors.New("Can't insert a value")
 	}
 
 	switch {
-	case value == n.Value:
+	case a.Name == n.Value.Name:
 		return nil
-
-	case value < n.Value:
+	case a.Name < n.Value.Name:
 		if n.Left == nil {
-			n.Left = &Node{Value: value, Data: data}
+			n.Left = &Node{Value: a}
 		}
-		return n.Left.Insert(value, data)
-
-	case value > n.Value:
+		return n.Left.Insert(a)
+	case a.Name > n.Value.Name:
 		if n.Right == nil {
-			n.Right = &Node{Value: value, Data: data}
+			n.Right = &Node{Value: a}
 		}
-		return n.Right.Insert(value, data)
+		return n.Right.Insert(a)
 	}
+
 	return nil
+}
+
+func (n *Node) Find(s string) (float64, bool) {
+	if n == nil {
+		return 0, false
+	}
+
+	switch {
+	case s == n.Value.Name:
+		return n.Value.Price, true
+	case s < n.Value.Name:
+		return n.Left.Find(s)
+	default:
+		return n.Right.Find(s)
+	}
 }
 
 func main() {
 
-	//head.val = 1
-	//insert(head, 2)
 	var head *Node
 	head = new(Node)
 
-	head.Insert("Litecoin", 12)
-	fmt.Println(head.Right.Data)
+	Litecoin := Altcoin{Name: "Litecoin", Price: 12.11}
+	head.Insert(Litecoin)
+	Decred := Altcoin{Name: "Decred", Price: 20.54}
+	head.Insert(Decred)
 
-	//	fmt.Println(head.val)
-	//fmt.Println(head.right.val)
-
+	fmt.Println(head.Right.Value.Price)
+	fmt.Println(head.Right.Left.Value.Price)
+	d := string("Decred")
+	var a float64
+	a, found := head.Find(d)
+	fmt.Println(a)
+	fmt.Println(found)
 }
